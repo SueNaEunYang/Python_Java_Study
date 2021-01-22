f7d251785c03abd19c91d8c314238be8b6db2b59
Sue Yang <artisticsue@naver.com>
2021-01-22 22:19:58 +0900

    fix : 전제 -> 전세로 고침 (오타 수정)

--- practice_9-12_quiz.py
+++ practice_9-12_quiz.py
@@ -13,7 +13,7 @@ class House:
         print("{0} {1} {2} {3} {4}".format(self.location, self.house_type, self.deal_type, self.price, self.completion_year))
 
 h1 = House("강남", "아파트", "매매", "10억", "2010년")
-h2 = House("마포", "오피스텔", "전제", "5억", "2007년")
+h2 = House("마포", "오피스텔", "전세", "5억", "2007년")
 h3 = House("송파", "빌라", "월세", "500/50", "2000년")
 house_lst =[]
 house_lst.append(h1)
@@ -41,7 +41,7 @@ h3.show_detail()
 
 # houses = []
 # house1 = House("강남", "아파트", "매매", "10억", "2010년")
-# house2 = House("마포", "오피스텔", "전제", "5억", "2007년")
+# house2 = House("마포", "오피스텔", "전세", "5억", "2007년")
 # house3 = House("송파", "빌라", "월세", "500/50", "2000년")
 # houses.append(house1)
 # houses.append(house2)
